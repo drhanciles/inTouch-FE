cd9dea091766222428725a8eeb66c8be4a8c631c
Derek Hanciles <drhanciles@icloud.com>
2019-02-16 12:17:03 -0700

    Implemented styling for CreateUser component, updating the layout of the form and adding additional stylings to the input and button

--- src/CreateUser/CreateUser.js
+++ src/CreateUser/CreateUser.js
@@ -16,12 +16,10 @@ export default class CreateUser extends Component {
     const { name, email, password, confirmation } = this.state
     return (
       <form>
-        <fieldset>
           <input className="create-user-input" name="name" value={name} type="text" required placeholder="Name"/>
           <input className="create-user-input" name="email" value={email} type="text" required placeholder="Email"/>
           <input className="create-user-input" name="password" value={password} type="password" required placeholder="Password"/>
           <input className="create-user-input" name="confirmation" value={confirmation} type="password" required placeholder="Confirmation"/>
-        </fieldset>
         <button className="create-account-button">Create Account</button>
       </form>
     )
