57ad284abb5eb530a2a1d1a659a3ebae2ba90a5f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-16 12:48:31 -0700

    Add ID attribute to Sign In User action in src/actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,10 @@ export const createUser = (name, email, password) => {
   }
 }
 
-export const signInUser = (email, password) => {
+export const signInUser = (id, email, password) => {
   return {
     type: 'SIGN_IN_USER', 
+    id,
     email, 
     password
   }
