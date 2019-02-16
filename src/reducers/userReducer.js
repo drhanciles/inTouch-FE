bf93d0656c1a09ed04cf10ceda59728b3cc9c094
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-16 14:18:38 -0700

    Modify syntax in src/reducers/userReducer.js file

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,20 +1,20 @@
-const user = (state={}, action) => {
+const userReducer = (state={}, action) => {
   switch (action.type) {
-    case 'CREATE_USER':
-      return {
-        state.id: action.id,
-        state.name: action.name
-      }
-    case 'SIGN_IN_USER':
-      return {
-        state.id: action.id,
-        state.name: action.name
-      }
-    case 'SIGN_OUT_USER': 
-      return {}
-    default: 
-      return state
+      case 'CREATE_USER':
+        return {
+          id: action.id,
+          name: action.name
+        }
+      case 'SIGN_IN_USER':
+        return {
+          id: action.id,
+          name: action.name
+        }
+      case 'SIGN_OUT_USER': 
+        return {}
+      default: 
+        return state
   }
 }
 
-export default user
+export default userReducer
