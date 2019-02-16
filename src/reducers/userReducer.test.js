f7d5e0528f46443b47e82d3f058306649a2b4aa0
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-16 15:16:33 -0700

    Add test for Create User state insrc/userReducer.test.js file

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -1,9 +1,29 @@
 import userReducer from './userReducer'
 
 describe('userReducer', () => {
+  
   it('should return the initial state', () => {
     const expected = {}
     const result = userReducer(undefined, {})
+
+    expect(result).toEqual(expected)
+  })
+  
+  const id = 1
+  const name = 'Derek'
+  const email = 'derek@email'
+  const password = 'password'
+  
+  it('should update state with user id and name when CREATE_USER case', () => {
+    const mockData = {name, email, password}
+    const action = {
+      type: 'CREATE_USER', 
+      data: mockData
+    }
+    
+    const expected = {id: action.id, name: action.name}
+
+    const result = userReducer({}, action)
 
     expect(result).toEqual(expected)
   })
