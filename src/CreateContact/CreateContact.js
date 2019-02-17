27348147fe61fb7ada8d4978c6bbd0a86d34cf07
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-17 13:47:57 -0700

    Modify html structure (tags), Add page header and save button

--- src/CreateContact/CreateContact.js
+++ src/CreateContact/CreateContact.js
@@ -18,32 +18,42 @@ export default class CreateContact extends Component {
     const { name, contactType, contactInformation, frequency, priority, notes } = this.state
     return (
       <main>
-        <article>
-          <form>
-            <input className="new-contact-name" name="name" value={name} type="text" required placeholder="Name"/>
-            <select className="contact-type" value={contactType} required> 
+        <div class="new-contact-header">New Contact</div>
+        <div class="article">
+          <div class="name">
+            <input class="new-contact-name" name="name" value={name} type="text" required placeholder="Name"/>
+          </div>
+          <div class="selections">
+            <select class="contact-type" value={contactType} required> 
               <option value="email">Email</option>
               <option value="call">Call</option>
               <option value="text">Text</option>
-            </selct>
-            <select className="priority" value={priority} required> 
+            </select>
+            <select class="priority" value={priority} required> 
               <option value="prio-1">1</option>
               <option value="prio-2">2</option>
               <option value="prio-3">3</option>
               <option value="prio-4">4</option>
               <option value="prio-5">5</option>
-            </selct>
-            <select className="frequency" value={priority} required> 
+            </select>
+            <select class="frequency" value={priority} required> 
               <option value="daily">Daily</option>
               <option value="weekly">Weekly</option>
               <option value="bi-weekly">Bi-Weekly</option>
               <option value="monthly">Monthly</option>
-            </selct>
-            <input className="contact-information" name="contact-information" value={contactInformation} type="text" required placeholder="Phone Number"/>
-            <label for="notes">Notes:</label>
-            <text-area value={notes} rows="5" name="notes" ></text-area>            
-          </form>
-        </article>
+            </select>
+          </div>
+          <div class="contact-info">
+            <input class="contact-information" name="contact-information" value={contactInformation} type="text" required placeholder="Phone Number"/>
+          </div>
+          <label for="notes">Notes:</label>
+          <div class="notes">
+            <text-area value={notes} rows="5" class="note-field" placeholder="Take your notes here."/>  
+          </div>          
+        </div>
+        <div class="save-contact">
+          <button class='save-contact-btn'>Save Contact</button>
+        </div>
       </main>
     )
   }
