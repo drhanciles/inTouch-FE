efd88b7f78e0079c139a2c80ef8cfffaaf249a5a
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-17 13:46:32 -0700

    Modify app size and structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import CreateUser from '../CreateUser/CreateUser.js'
+import CreateContact from '../CreateContact/CreateContact.js'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-      <header>
-        <h1>inTouch.</h1>
-      </header>
-        <CreateUser />
+        <header>
+          <h1>inTouch.</h1>
+        </header>
+        <body>
+          <CreateContact />
+        </body>
       </div>
     );
   }
