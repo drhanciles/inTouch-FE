04918ca60ef9077690ea9b86fc9f59d0c1e71df9
Derek Hanciles <drhanciles@icloud.com>
2019-02-17 14:25:50 -0700

    Updated issue that was causing background color to be cut off at certain componets. Issue has been resolved

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import CreateUser from '../CreateUser/CreateUser.js'
+// import CreateUser from '../CreateUser/CreateUser.js'; 
+import HomePage from '../HomePage/HomePage.js'; 
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-      <header>
+      {/* <header>
         <h1>inTouch.</h1>
       </header>
-        <CreateUser />
+        <CreateUser /> */}
+      <HomePage />
       </div>
     );
   }
