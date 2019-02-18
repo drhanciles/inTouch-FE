7fa8d2a55f39c388ffa6683c3e5dbf83caf61a65
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 14:08:16 -0700

    Create test for Has Errored Reducer

--- src/reducers/hasErroredReducer.test.js
+++ src/reducers/hasErroredReducer.test.js
@@ -8,4 +8,16 @@ describe('hasErroredReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  
+  it('should update the state when Has Errored case', () => {
+    const action = {
+      type: 'HAS_ERRORED',
+      hasErrored: true
+    }
+    const expected = true
+    
+    const result = hasErroredReducer(false, action)
+    
+    expect(result).toEqual(expected)
+  })
 })
