e405bfdf41b0b8c887364520d194e6620dec102c
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 13:40:23 -0700

    Add test for isLoading action

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -44,4 +44,17 @@ describe('actions', () => {
   })
   
   
+  it('should return a type of IS_LOADING', () => {
+    const isLoading = true
+    const expectedAction = {
+      type: 'IS_LOADING',
+      isLoading
+    }
+    
+    const result = actions.isLoading(isLoading)
+    
+    expect(result).toEqual(expectedAction)
+  })
+  
+  
 })
