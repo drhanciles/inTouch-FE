41a92b6027b800bc8d5ae689e1d775ced5449aa8
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 13:35:02 -0700

    Update Sign In User action and comment out Create User action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,27 @@
-export const createUser = (name, email, password) => ({
-  type: 'CREATE_USER',
-  name, 
-  email, 
-  password
-})
+// export const createUser = (userName, email, password) => ({
+//   type: 'CREATE_USER',
+//   userName, 
+//   email, 
+//   password
+// })
 
-export const signInUser = (email, password) => ({
+export const signInUser = (id, userName, token) => ({
   type: 'SIGN_IN_USER',
-  email, 
-  password
+  id,
+  userName, 
+  token
 })
 
 export const signOutUser = () => ({
   type: 'SIGN_OUT_USER'
 })
 
+export const isLoading = (check) => ({
+  type: 'IS_LOADING',
+  isLoading: check
+})
+
+export const hasErrored = (check) => ({
+  type: 'HAS_ERRORED',
+  hasErrored: check
+})
