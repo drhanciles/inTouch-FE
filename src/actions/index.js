4dc3f8049f01b5e63166eaca21c393f932d4088f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 13:26:55 -0700

    Refactor User actions in src/actions/index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,17 @@
-export const createUser = (name, email, password) => {
-  return {
-    type: 'CREATE_USER',
-    name, 
-    email, 
-    password
-  }
-}
+export const createUser = (name, email, password) => ({
+  type: 'CREATE_USER',
+  name, 
+  email, 
+  password
+})
 
-export const signInUser = (email, password) => {
-  return {
-    type: 'SIGN_IN_USER',
-    email, 
-    password
-  }
-}
+export const signInUser = (email, password) => ({
+  type: 'SIGN_IN_USER',
+  email, 
+  password
+})
 
-export const signOutUser = () => {
-  return {
-    type: 'SIGN_OUT_USER'
-  }
-}
+export const signOutUser = () => ({
+  type: 'SIGN_OUT_USER'
+})
+
