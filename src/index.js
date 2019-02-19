eff36e06d94c73d7aac868a00675ce9571b102b6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 20:19:22 -0700

    Connect Redux Store to App

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App.js';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/index.js';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const wrapper = (
+ <Provider store={store}>
+     <App/>
+ </Provider>
+)
+
+ReactDOM.render(wrapper, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
