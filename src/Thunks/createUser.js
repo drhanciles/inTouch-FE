6b6df7775fd08e2bf2fdb8b8c2503c8ac68dd63c
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-19 10:59:50 -0700

    save no changes

---
+++ src/Thunks/createUser.js
@@ -0,0 +1,30 @@
+import { isLoading, hasErrored, signInUser } from '../Actions/index.js'
+import { authenticateUser } from './authenticateUser'
+require('isomorphic-fetch');
+
+export const createUser = (userName, email, password) => {
+  const url = 'https://in-touch-dev.herokuapp.com/api/v1/data/'
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({query: "mutation { createUser(email, userName, password){user {userName}}}"}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      if (!response.ok) {
+        dispatch(isLoading(false))
+        throw Error(response.statusText)
+      }
+        dispatch(isLoading(false))
+      const data = await response.json()
+      const name = data.data.createUser.user.username
+      const token = await authenticateUser(name, password)
+        dispatch(signInUser(name, token))
+    } catch (error) {
+        dispatch(hasErrored(true))
+    }
+  }
+}
