277f3de22cae91a626873aa52b842ea92cfde253
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-18 20:41:33 -0700

    Refactor CreateUser.js file and add disabled state to submit button

--- src/CreateUser/CreateUser.js
+++ src/CreateUser/CreateUser.js
@@ -8,21 +8,22 @@ export default class CreateUser extends Component {
   constructor() {
     super()
     this.state = {
-      name: '', 
+      userName: '', 
       email: '', 
       password: '',
-      confirmation: ''
+      confirmation: '', 
+      disabled: true
     }
   }
   render() {
-    const { name, email, password, confirmation } = this.state
+    const { userName, email, password, confirmation, disabled } = this.state
     return (
       <form>
-          <input className="new-user-name" name="name" value={name} type="text" required placeholder="Name"/>
+          <input className="new-user-name" name="userName" value={userName} type="text" required placeholder="Username"/>
           <input className="new-user-email" name="email" value={email} type="text" required placeholder="Email"/>
           <input className="new-user-password" name="password" value={password} type="password" required placeholder="Password"/>
           <input className="new-user-password-confirmation" name="confirmation" value={confirmation} type="password" required placeholder="Confirmation"/>
-        <button className="new-account-button">Create Account</button>
+        <button className="new-account-button" disabled={disabled}>Create Account</button>
       </form>
     )
   }
