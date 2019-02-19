3c52fdf76f596dd2b7013c66ee1dda3d7c066435
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-19 11:17:33 -0700

    Add test to check if handleChange has been called

--- src/CreateUser/CreateUser.test.js
+++ src/CreateUser/CreateUser.test.js
@@ -1,23 +1,40 @@
 import React from 'react'; 
 import { shallow } from 'enzyme'; 
-import CreateUser from './CreateUser.js'; 
+import { CreateUser, mapDispatchToProps } from './CreateUser.js'; 
+import { createUser } from '../Thunks/createUser.js'
+
 
 describe('CreateUser', () => {
   let mockName; 
   let mockEmail; 
   let mockPassword; 
   let mockConfirmation; 
+  let mockEvent
+  let wrapper
   
   beforeEach(() => {
+    wrapper = shallow(<CreateUser createNewUser={jest.fn()}/>)
     mockName = 'Kaytranada'; 
     mockEmail = 'yaboy@ymail.com'; 
     mockPassword = 'password'; 
-    mockConfirmation = 'password'
+    mockConfirmation = 'password';
+    mockEvent = {
+     target: {
+       name: 'userName',
+       value: 'Derek'
+      }
+    }
   })
   
   it('should match snapshot', () => {
-    const wrapper = shallow(<CreateUser createNewUser={jest.fn()}/>)
-
+    
     expect(wrapper).toMatchSnapshot()
-  });
-});
+  })
+  
+  it('should update state when handleChange occures on an input', () => {
+    wrapper.handleChange = jest.fn()
+    wrapper.find('.new-user-name').simulate('change', mockEvent)
+    
+    expect(wrapper.handleChange).toHaveBeenCalled
+  })
+})
