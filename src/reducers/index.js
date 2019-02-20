feea0568e6f582153183cb67448217010d23d807
Derek Hanciles <drhanciles@icloud.com>
2019-02-20 13:42:55 -0700

    Updated combine reducer to now store selection from the user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { isLoadingReducer } from './isLoadingReducer.js'
 import { hasErroredReducer } from './hasErroredReducer.js'
 import { userReducer } from './userReducer.js'
+import { selectionReducer } from './selectionReducer.js'
 
 export const rootReducer = combineReducers({
   loading: isLoadingReducer, 
   errored: hasErroredReducer, 
+  selection: selectionReducer,
   user: userReducer
 })
