aef299364295048ac513915a786961528721d773
Derek Hanciles <drhanciles@icloud.com>
2019-02-22 17:28:26 -0700

    Updated styling for HomePage component

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -14,20 +14,35 @@ export class HomePage extends Component {
   render() {
     const { user } = this.props
     return (
-
       <main className="home-page">
-        <article className="message-container">
-          <h2 className="reachout-message-intro">Hello,</h2>
+        <div className="message-container">
+          <h2 className="reachout-message-intro">Hello Rajaa,</h2>
           <h2 className="reachout-message-content">Have You Called Mom Today?</h2>
-        </article>
-        <article className="message-buttons-container">
+        </div>
+        <div className="buttons-container">
           <button className="done-button">Done</button>
           <button className="reminder-button">Remind Me Tomorrow</button>
-        </article>
-      </main>    
-      )
+        </div>
+        <div className="occasions-label">Upcoming Occasions:</div>
+        <div className="occasions-container"> 
+          <div className="occasion">
+            <div className="date-container">01/03/19</div>
+            <div className="occasaion-reminder-content">Celebrate Graduation with family dinner</div>
+          </div>
+          <div className="occasion">
+            <div className="date-container">18/04/19</div>
+            <div className="occasaion-reminder-content"> Mom's Birthday</div>
+          </div>
+          <div className="occasion">
+            <div className="date-container">05/12/19</div>
+            <div className="occasaion-content">Mother's Day dinner at Mama Mia</div>
+          </div>
+        </div>
+      </main>
+  )
   }
 }
+          
 
 export const mapStateToProps = (state) => ({
   user: state.user,
