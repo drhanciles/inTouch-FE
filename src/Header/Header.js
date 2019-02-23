efa9273a91d843fe4af280141a85b286444ba8fe
Derek Hanciles <drhanciles@icloud.com>
2019-02-22 17:35:05 -0700

    Imported Header css into the Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import  { connect } from 'react-redux'; 
 import { setSelection } from '../actions/index.js'; 
+import './Header.css'; 
 
 export class Header extends Component {
   constructor() {
