12cae98f824f729ae05f5ba3e14c4e2e64e04340
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-23 17:41:53 -0700

    Add Create Contact case to contactsReducer.js

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -2,6 +2,8 @@ export const contactsReducer = (state=[], action) => {
   switch (action.type) {
     case 'GET_ALL_CONTACTS':
       return action.contacts
+    case 'CREATE_CONTACT':
+      return action.cantact
     case 'GET_CURRENT_CONTACT': 
     default: 
       return state
