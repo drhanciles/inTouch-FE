392580638c4cf762adb161b541228b0fc6dc7870
Derek Hanciles <drhanciles@icloud.com>
2019-02-23 18:20:44 -0700

    Implemented conditional rendering in HomePage component based on the click of the done and reminder button

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -19,35 +19,60 @@ export class HomePage extends Component {
     suggestedContacts(token)
   }
 
-  completedReachOut = (e) => {
-    e.preventDefault()
+  completedReachOut = () => {
     const { user } = this.props
+    this.setState({
+      doneButtonClicked: true
+    })
     return (
-      <h2 className="reachout-message-content">{user.name} you are all caught up, come back tomorrow for your next contact to stay in touch with!</h2>
+      <article className="message-container">
+        <h2 className="reachout-message-content">{user.name} you are all caught up, come back tomorrow for your next contact to stay in touch with!</h2>
+      </article>
     )
   }
 
-  remindUserTomorrow = (e) => {
-    e.preventDefault()
+  remindUserTomorrow = () => {
     const { user } = this.props
+    this.setState({
+      reminderButtonClicked: true
+    })
     return (
-      <h2 className="reachout-message-content">{user.name} no worries come back tomorrow when you have time.</h2>
+      <article className="message-container">
+        <h2 className="reachout-message-content">{user.name} no worries come back tomorrow when you have time.</h2>
+      </article>
     )
 
   }
 
   render() {
     const { user } = this.props
+    const { doneButtonClicked, reminderButtonClicked } = this.state
+    let content
+    const noButtonsSelected = (
+                              <article>
+                                <article className="message-container">
+                                  <h2 className="reachout-message-intro">Hello {user.name},</h2>
+                                  <h2 className="reachout-message-content">Have You Called Mom Today?</h2>
+                                </article>
+                                <div className="buttons-container">
+                                  <button onClick={ this.completedReachOut } className="done-button">Done</button>
+                                  <button onClick={ this.remindUserTomorrow } className="reminder-button">Remind Me Tomorrow</button>
+                                </div>
+                              </article>
+                            )
+    const doneSelected = this.completedReachOut()
+    const reminderSelected = this.remindUserTomorrow()
+    if (doneButtonClicked) {
+      content = doneSelected
+    } else if (reminderButtonClicked) {
+      content = reminderSelected
+    } else {
+      content = noButtonsSelected
+    }
+
     return (
       <main className="home-page">
-        <div className="message-container">
-          <h2 className="reachout-message-intro">Hello {user.name},</h2>
-          <h2 className="reachout-message-content">Have You Called Mom Today?</h2>
-        </div>
-        <div className="buttons-container">
-          <button onClick={ this.completedReachOut } className="done-button">Done</button>
-          <button onClick={ this.remindUserTomorrow } className="reminder-button">Remind Me Tomorrow</button>
-        </div>
+        {content}
         <div className="occasions-label">Upcoming Occasions:</div>
         <div className="occasions-container"> 
           <div className="occasion">
